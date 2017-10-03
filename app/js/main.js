87f230f3f3556328f79165e274500102a423c90d
joncgrubb <joncgrubbdev@gmail.com>
2017-10-03 11:51:02 -0400

    added v-bind for class in html; logic in js

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,42 @@
 var menu = new Vue({
 	el: '#menu',
 	data: {
-		message: 'Please click a menu item'
+		message: 'Please click a menu item',
+		homeActive: false,
+		projectsActive: false,
+		servicesActive: false,
+		contactActive: false
 	},
 	methods: {
-
+		makeActive: function(item, label) {
+			//console.log('makeActive');
+			//console.log('makeActive - item:', item, 'label:', label);
+			// Remove active class from previous anchor
+			this.homeActive = false;
+			this.projectsActive = false;
+			this.servicesActive = false;
+			this.contactActive = false;
+			// Add active class to the selected anchor (item)
+			switch(item) {
+				case 'homeMenu':
+				this.homeActive = true;
+				//console.log('Colorize:', label);
+				break;
+				case 'projectsMenu':
+				this.projectsActive = true;
+				//console.log('Colorize:', label);
+				break;
+				case 'servicesMenu':
+				this.servicesActive = true;
+				//console.log('Colorize:', label);
+				break;
+				case 'contactMenu':
+				this.contactActive = true;
+				//console.log('Colorize:', label);
+				break;
+			}
+			// Change text of message
+			this.message = 'You chose ' + label;
+		}
 	}
 });
